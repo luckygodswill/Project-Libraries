206d286b5ae1a322d21a7470995b84573a73c3f8
luckygodswill <lucky_godswill@yahoo.com>
2015-11-05 14:45:38 +0000

    Update i2cmaster.cpp

--- i2cmaster.cpp
+++ i2cmaster.cpp
@@ -1,231 +1,81 @@
-/*************************************************************************
-* Title:    I2C master library using hardware TWI interface
-* Author:   Peter Fleury <pfleury@gmx.ch>  http://jump.to/fleury
-* File:     $Id: twimaster.c,v 1.3 2005/07/02 11:14:21 Peter Exp $
-* Software: AVR-GCC 3.4.3 / avr-libc 1.2.3
-* Target:   any AVR device with hardware TWI 
-* Usage:    API compatible with I2C Software Library i2cmaster.h
-**************************************************************************/
+/*************************************************** 
+  This is a library for the MLX90614 Temp Sensor
+  Designed specifically to work with the MLX90614 sensors in the
+  adafruit shop
+  ----> https://www.adafruit.com/products/1748
+  ----> https://www.adafruit.com/products/1749
+  These sensors use I2C to communicate, 2 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
 
-// In order to work around strange bugs in the arduino software, this library
-// has been wrapped in a simple i2cmaster class. Modification performed by
-// Matthew Beckler and Adam Wolf of Wayne and Layne, LLC, for their 
-// Video Game Shield project:
-// http://www.wayneandlayne.com/projects/video-game-shield/
+#include "Adafruit_MLX90614.h"
 
-#ifndef I2CMASTER_C
-#define I2CMASTER_C
-
-#include <inttypes.h>
-#include <compat/twi.h>
-
-#if defined(ARDUINO) && ARDUINO >= 100
-#include "Arduino.h"
-#else
-#include "WProgram.h"
-#endif
-
-#include "i2cmaster.h"
+Adafruit_MLX90614::Adafruit_MLX90614(uint8_t i2caddr) {
+  _addr = i2caddr;
+}
 
 
-/* define CPU frequency in Mhz here if not defined in Makefile */
-#ifndef F_CPU
-#define F_CPU 1600000UL
-#endif
+boolean Adafruit_MLX90614::begin(void) {
+  Wire.begin();
 
-/* I2C clock in Hz */
-#define SCL_CLOCK  100000L
+  /*
+  for (uint8_t i=0; i<0x20; i++) {
+    Serial.print(i); Serial.print(" = ");
+    Serial.println(read16(i), HEX);
+  }
+  */
+  return true;
+}
 
-/*************************************************************************
- Initialization of the I2C bus interface. Need to be called only once
-*************************************************************************/
-void i2cmaster::i2c_init(void)
-{
-  /* initialize TWI clock: 100 kHz clock, TWPS = 0 => prescaler = 1 */
+//////////////////////////////////////////////////////
+
+
+double Adafruit_MLX90614::readObjectTempF(void) {
+  return (readTemp(MLX90614_TOBJ1) * 9 / 5) + 32;
+}
+
+
+double Adafruit_MLX90614::readAmbientTempF(void) {
+  return (readTemp(MLX90614_TA) * 9 / 5) + 32;
+}
+
+double Adafruit_MLX90614::readObjectTempC(void) {
+  return readTemp(MLX90614_TOBJ1);
+}
+
+
+double Adafruit_MLX90614::readAmbientTempC(void) {
+  return readTemp(MLX90614_TA);
+}
+
+float Adafruit_MLX90614::readTemp(uint8_t reg) {
+  float temp;
   
-  TWSR = 0;                         /* no prescaler */
-  TWBR = ((F_CPU/SCL_CLOCK)-16)/2;  /* must be > 10 for stable operation */
+  temp = read16(reg);
+  temp *= .02;
+  temp  -= 273.15;
+  return temp;
+}
 
-  // This line added by Matthew Beckler (Wayne and Layne, LLC) to work with
-  // designs that don't have external pullup resistors. This command turns on
-  // the internal pullups. Might not work if you have large or long wires for
-  // SDA and SCL, or if you have many devices on the bus. Works just fine with
-  // a 24LC256 EEPROM on a pcb with the atmega328p.
-  PORTC |= _BV(4) | _BV(5);
-}/* i2c_init */
+/*********************************************************************/
 
+uint16_t Adafruit_MLX90614::read16(uint8_t a) {
+  uint16_t ret;
 
-/*************************************************************************	
-  Issues a start condition and sends address and transfer direction.
-  return 0 = device accessible, 1= failed to access device
-*************************************************************************/
-unsigned char i2cmaster::i2c_start(unsigned char address)
-{
-    uint8_t   twst;
-
-    // send START condition
-    TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
-
-    // wait until transmission completed
-    while(!(TWCR & (1<<TWINT)));
-
-    // check value of TWI Status Register. Mask prescaler bits.
-    twst = TW_STATUS & 0xF8;
-    if ( (twst != TW_START) && (twst != TW_REP_START)) return 1;
-
-    // send device address
-    TWDR = address;
-    TWCR = (1<<TWINT) | (1<<TWEN);
-
-    // wail until transmission completed and ACK/NACK has been received
-    while(!(TWCR & (1<<TWINT)));
-
-    // check value of TWI Status Register. Mask prescaler bits.
-    twst = TW_STATUS & 0xF8;
-    if ( (twst != TW_MT_SLA_ACK) && (twst != TW_MR_SLA_ACK) ) return 1;
-
-    return 0;
-
-}/* i2c_start */
-
-
-/*************************************************************************
- Issues a start condition and sends address and transfer direction.
- If device is busy, use ack polling to wait until device is ready
- Provide the timeout you want in timeout_ms.
- Returns:
-    0 device accessible
-    1 timeout
- 
- Input:   address and transfer direction of I2C device, timeout_ms
-*************************************************************************/
-unsigned char i2cmaster::i2c_start_wait(unsigned char address, unsigned int timeout_ms)
-{
-    uint8_t   twst;
-
-    unsigned long end_time = millis() + timeout_ms;
-
-    while ( millis() < end_time )
-    {
-        // send START condition
-        TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
-    
-    	// wait until transmission completed
-    	while(!(TWCR & (1<<TWINT)));
-    
-    	// check value of TWI Status Register. Mask prescaler bits.
-    	twst = TW_STATUS & 0xF8;
-    	if ( (twst != TW_START) && (twst != TW_REP_START)) continue;
-    
-    	// send device address
-    	TWDR = address;
-    	TWCR = (1<<TWINT) | (1<<TWEN);
-    
-    	// wail until transmission completed
-    	while(!(TWCR & (1<<TWINT)));
-    
-    	// check value of TWI Status Register. Mask prescaler bits.
-    	twst = TW_STATUS & 0xF8;
-    	if ( (twst == TW_MT_SLA_NACK )||(twst ==TW_MR_DATA_NACK) ) 
-    	{    	    
-    	    /* device busy, send stop condition to terminate write operation */
-            TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
-            
-            // wait until stop condition is executed and bus released
-            while(TWCR & (1<<TWSTO));
-
-    	    continue;
-    	}
-	        
-    	//if( twst != TW_MT_SLA_ACK) return 1;
-    	return 0;
-    }
-    return 1;
-
-}/* i2c_start_wait */
-
-
-/*************************************************************************
- Issues a repeated start condition and sends address and transfer direction 
- Input:   address and transfer direction of I2C device
- 
- Return:  0 device accessible
-          1 failed to access device
-*************************************************************************/
-unsigned char i2cmaster::i2c_rep_start(unsigned char address)
-{
-    return i2c_start( address );
-
-}/* i2c_rep_start */
-
-
-/*************************************************************************
- Terminates the data transfer and releases the I2C bus
-*************************************************************************/
-void i2cmaster::i2c_stop(void)
-{
-    /* send stop condition */
-	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
-	
-	// wait until stop condition is executed and bus released
-	while(TWCR & (1<<TWSTO));
-
-}/* i2c_stop */
-
-
-/*************************************************************************
-  Send one byte to I2C device
+  Wire.beginTransmission(_addr); // start transmission to device 
+  Wire.write(a); // sends register address to read from
+  Wire.endTransmission(false); // end transmission
   
-  Input:    byte to be transfered
-  Return:   0 write successful 
-            1 write failed
-*************************************************************************/
-unsigned char i2cmaster::i2c_write( unsigned char data )
-{	
-    uint8_t   twst;
-    
-	// send data to the previously addressed device
-	TWDR = data;
-	TWCR = (1<<TWINT) | (1<<TWEN);
+  Wire.requestFrom(_addr, (uint8_t)3);// send data n-bytes read
+  ret = Wire.read(); // receive DATA
+  ret |= Wire.read() << 8; // receive DATA
 
-	// wait until transmission completed
-	while(!(TWCR & (1<<TWINT)));
+  uint8_t pec = Wire.read();
 
-	// check value of TWI Status Register. Mask prescaler bits
-	twst = TW_STATUS & 0xF8;
-	if( twst != TW_MT_DATA_ACK) return 1;
-	return 0;
-
-}/* i2c_write */
-
-
-/*************************************************************************
- Read one byte from the I2C device, request more data from device 
- 
- Return:  byte read from I2C device
-*************************************************************************/
-unsigned char i2cmaster::i2c_readAck(void)
-{
-	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWEA);
-	while(!(TWCR & (1<<TWINT)));    
-
-    return TWDR;
-
-}/* i2c_readAck */
-
-
-/*************************************************************************
- Read one byte from the I2C device, read is followed by a stop condition 
- 
- Return:  byte read from I2C device
-*************************************************************************/
-unsigned char i2cmaster::i2c_readNak(void)
-{
-	TWCR = (1<<TWINT) | (1<<TWEN);
-	while(!(TWCR & (1<<TWINT)));
-	
-    return TWDR;
-
-}/* i2c_readNak */
-
-#endif
+  return ret;
+}
